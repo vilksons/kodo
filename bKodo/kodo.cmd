497179de3f1979186d0fd62f709ebe8cb9d914c2
vilksons <barokahsukawangi@gmail.com>
2025-04-09 12:12:30 +0700

    bkodo

--- bKodo/kodo.cmd
+++ bKodo/kodo.cmd
@@ -1,59 +1,8 @@
-:: Contributors 2024-2025 Anonim "Socket"
-:: thanks for using this program.
-
 @ECHO OFF
 
 setlocal EnableDelayedExpansion
 
 color F
-
-:: pawncc 3.10.7^
-:: Usage:   pawncc <filename> [filename...] [options]
-:: Options:
-::         -A<num>  alignment in bytes of the data segment and the stack
-::         -a       output assembler code
-::         -C[+/-]  compact encoding for output file (default=+)
-::         -c<name> codepage name or number; e.g. 1252 for Windows Latin-1
-::         -Dpath   active directory path
-::         -d<num>  debugging level (default=-d1)
-::             0    no symbolic information, no run-time checks
-::             1    run-time checks, no symbolic information
-::             2    full debug information and dynamic checking
-::             3    same as -d2, but implies -O0
-::         -e<name> set name of error file (quiet compile)
-::         -H<hwnd> window handle to send a notification message on finish
-::         -i<name> path for include files
-::         -l       create list file (preprocess only)
-::         -o<name> set base name of (P-code) output file
-::         -O<num>  optimization level (default=-O1)
-::             0    no optimization
-::             1    JIT-compatible optimizations only
-::             2    full optimizations
-::         -p<name> set name of "prefix" file
-::         -R[+/-]  add detailed recursion report with call chains (default=-)
-::         -r[name] write cross reference report to console or to specified file
-::         -S<num>  stack/heap size in cells (default=4096)
-::         -s<num>  skip lines from the input file
-::         -t<num>  TAB indent size (in character positions, default=8)
-::         -v<num>  verbosity level; 0=quiet, 1=normal, 2=verbose (default=1)
-::         -w<num>  disable a specific warning by its number
-::         -X<num>  abstract machine size limit in bytes
-::         -XD<num> abstract machine data/stack size limit in bytes
-::         -Z[+/-]  run in compatibility mode (default=-)
-::         -E[+/-]  turn warnings in to errors
-::         -\       use '\' for escape characters
-::         -^       use '^' for escape characters
-::         -;[+/-]  require a semicolon to end each statement (default=-)
-::         -([+/-]  require parantheses for function invocation (default=-)
-::         sym=val  define constant "sym" with value "val"
-::         sym=     define constant "sym" with value 0
-
-:: Options may start with a dash or a slash; the options "-d0" and "/d0" are
-:: equivalent.
-
-:: Options with a value may optionally separate the value from the option letter
-:: with a colon (":") or an equal sign ("="). That is, the options "-d0", "-d=0"
-:: and "-d:0" are all equivalent.
 
 SET "ASM_OPTION_M=-o"
 SET "ASM_OPTION_P=-C- -O0 -d2"
@@ -82,7 +31,7 @@ FOR /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & ECHO on & FOR %%b in (1)
 
 :NEXT
 SET "BATCHFILE=true"
-SET "BATCHOPTION=cat"
+SET "BATCHOPTION=kodo"
 
 IF "%KODOTYPEOF%"=="%BATCHOPTION% -c" (
 
@@ -440,19 +389,19 @@ IF "%KODOTYPEOF%"=="%BATCHOPTION% -c" (
     TITLE user:~/!BATCHTITLE!
     
 :HELPL
-    ECHO usage: cat [-c compile] [-r running] [-t test server] [-ci compile-running] 
+    ECHO usage: kodo [-c compile] [-r running] [-t test server] [-ci compile-running] 
     ECHO       	   [-F folder check] [-C clear screen] [-P pawncc release] [-V vscode tasks]
     ECHO       	   [-X clone sampKodo] [-R rename file] [-K kill batch] [-D directory]
     ECHO       	   [-N netstat] [-I ipconfig] [-S systeminfo] [-G git init]
     GOTO COMMAND_TYPEOF
 
-) ELSE IF "%KODOTYPEOF%"=="cat ." (
+) ELSE IF "%KODOTYPEOF%"=="kodo ." (
 
     GOTO TESTSERVERS
 
-) ELSE IF "%KODOTYPEOF%"=="cat" (
+) ELSE IF "%KODOTYPEOF%"=="kodo" (
 
-    SET "BATCHTITLE=cat"
+    SET "BATCHTITLE=kodo"
     TITLE user:~/!BATCHTITLE!
     GOTO HELPL
 
